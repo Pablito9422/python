50eaa370f2b302990ac0b821e8d8d3cda4e824e3
David Smith <smithdc@gmail.com>
2024-04-03 15:16:22 +0100

    Test edits

--- tests/gis_tests/gdal_tests/test_geom.py
+++ tests/gis_tests/gdal_tests/test_geom.py
@@ -1018,7 +1018,7 @@ class OGRGeomTest(SimpleTestCase, TestDataMixin):
     def test_curvepolygon_has_polygon_features(self):
         geom = OGRGeometry(
             "CURVEPOLYGON ZM (CIRCULARSTRING ZM (0 0 0 0, 4 0 0 0, 4 4 0 0, 0 4 0 0, "
-            "0 0 0 0), (1 1 0 0, 3 3 1 0, 3 1 1 0, 1 1 2 0))"
+            "0 0 0 0), (1 1 0 0, 3 3 0 0, 3 1 0 0, 1 1 0 0))"
         )
         self.assertIsInstance(geom, CurvePolygon)
         self.assertIsInstance(geom.shell, CircularString)
