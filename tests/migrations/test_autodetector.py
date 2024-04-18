c51fb532732d2b54244fbaeb25c3edbc8f70eaa2
bcail <bcail@crossway.org>
2024-04-18 10:17:52 -0400

    Update tests/migrations/test_autodetector.py
    Co-authored-by: Bhuvnesh <bhuvnesh875@gmail.com>

--- tests/migrations/test_autodetector.py
+++ tests/migrations/test_autodetector.py
@@ -1362,7 +1362,7 @@ class AutodetectorTests(BaseAutodetectorTests):
             "testapp",
             "Author",
             [
-                ("id", models.BigAutoField()),
+                ("id", models.AutoField(primary_key=True)),
                 ("pkfield", models.IntegerField(primary_key=False)),
             ],
         )
