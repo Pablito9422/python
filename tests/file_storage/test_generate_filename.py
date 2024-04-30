f3ac75f3e0aa6a421ace79888dfb0412fded01e5
Ben Cail <bcail@crossway.org>
2024-04-30 13:42:37 -0400

    test suspicious operation for overwriting storage

--- tests/file_storage/test_generate_filename.py
+++ tests/file_storage/test_generate_filename.py
@@ -60,14 +60,19 @@ class GenerateFilenameStorageTests(SimpleTestCase):
             ("/tmp/../path", "/tmp/.."),
             ("\\tmp\\..\\path", "/tmp/.."),
         ]
-        s = FileSystemStorage()
+        storage = FileSystemStorage()
+        overwrite_storage = FileSystemStorage(allow_overwrite=True)
         for file_name, path in candidates:
             msg = "Detected path traversal attempt in '%s'" % path
             with self.subTest(file_name=file_name):
                 with self.assertRaisesMessage(SuspiciousFileOperation, msg):
-                    s.get_available_name(file_name)
+                    storage.get_available_name(file_name)
                 with self.assertRaisesMessage(SuspiciousFileOperation, msg):
-                    s.generate_filename(file_name)
+                    storage.generate_filename(file_name)
+                with self.assertRaisesMessage(SuspiciousFileOperation, msg):
+                    overwrite_storage.get_available_name(file_name)
+                with self.assertRaisesMessage(SuspiciousFileOperation, msg):
+                    overwrite_storage.generate_filename(file_name)
 
     def test_filefield_dangerous_filename(self):
         candidates = [
