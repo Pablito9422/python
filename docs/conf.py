7730b3ae3e54b8b5d5fa1f51bdc287004d63d221
Natalia <124304+nessita@users.noreply.github.com>
2024-05-03 13:56:50 -0300

    Bumped version; main is now 5.2 pre-alpha.

--- docs/conf.py
+++ docs/conf.py
@@ -111,7 +111,7 @@ copyright = "Django Software Foundation and contributors"
 # built documents.
 #
 # The short X.Y version.
-version = "5.1"
+version = "5.2"
 # The full version, including alpha/beta/rc tags.
 try:
     from django import VERSION, get_version
@@ -128,7 +128,7 @@ else:
     release = django_release()
 
 # The "development version" of Django
-django_next_version = "5.1"
+django_next_version = "5.2"
 
 extlinks = {
     "bpo": ("https://bugs.python.org/issue?@action=redirect&bpo=%s", "bpo-%s"),
